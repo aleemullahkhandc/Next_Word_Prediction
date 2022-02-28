30ce465323255295f8af116a9654b0e42e170994
Shail079 <91492700+Shail079@users.noreply.github.com>
2022-02-27 22:31:27 -0500

    Trying to get input from after developing JS File

--- app.py
+++ app.py
@@ -11,21 +11,21 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/get_end_predictions', methods=['post'])
+@app.route('/get_end_predictions', methods=['post'])  #after creating JSON file
 def get_prediction_eos():
     try:
         input_text = ' '.join(request.json['input_text'].split())
         input_text += ' <mask>'
         top_k = request.json['top_k']
         res = main.get_all_predictions(input_text, top_clean=int(top_k))
-        return app.response_class(response=json.dumps(res), status=200, mimetype='application/json')
+        return app.response_class(response=json.dumps(res), status=200, mimetype='application/json') 
     except Exception as error:
         err = str(error)
         print(err)
         return app.response_class(response=json.dumps(err), status=500, mimetype='application/json')
 
 
-@app.route('/get_mask_predictions', methods=['post'])
+@app.route('/get_mask_predictions', methods=['post'])   #after creating JSON file
 def get_prediction_mask():
     try:
         input_text = ' '.join(request.json['input_text'].split())
